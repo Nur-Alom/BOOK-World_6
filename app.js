ae255bb8415267d430f93191fe63a91f16fcecc3
Nur-Alom <alamn7150@gmail.com>
2021-09-02 16:13:58 +0600

    3rd commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const searchValue = () => {
     // console.log(searchText);
     searchInput.value = '';
 
-    const url = `http://openlibrary.org/search.json?q=${searchValue}`;
+    const url = `https://openlibrary.org/search.json?q=${searchValue}`;
     // console.log(url);
     fetch(url)
         .then(res => res.json())
@@ -16,9 +16,24 @@ const searchValue = () => {
 
 const displaySearchResult = (books) => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (books.length === 0 || books.length === '') {
+        document.getElementById('error').innerText = 'No Result Found!!';
+    }
     books.forEach(book => {
-        const div = document.createElement('div')
-        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <div class="card-body">
+                <h3 class="text-warning">Book Name: ${book.title}</h3>
+                <h5>Author Name: ${book.author_name}</h5>
+                <p><strong class="text-success">First Publish Year: ${book.first_publish_year}</strong></p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+        // console.log(book);
     })
 }
 
